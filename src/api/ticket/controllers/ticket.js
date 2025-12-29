14079cf4bec44434482a8aecfb60804c3d5fd38e
ajay.t <ajaythekkiniyi@gmail.com>
2025-12-29 16:14:59 +0530

    admin can see all tickets, other users can see only cereated or assigned

--- src/api/ticket/controllers/ticket.js
+++ src/api/ticket/controllers/ticket.js
@@ -1,15 +1,82 @@
 'use strict';
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::ticket.ticket', ({ strapi }) => ({
+
   async create(ctx) {
-    // This executes the default Strapi logic (saving to DB + uploading files)
     const response = await super.create(ctx);
 
-    // This sends the 201 Created status back to your React app
-    return ctx.send({
-      message: "Ticket created successfully",
-      data: response.data
-    }, 201); 
+    return ctx.send(
+      {
+        message: 'Ticket created successfully',
+        data: response.data,
+      },
+      201
+    );
   },
+
+  async find(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized('You must be logged in');
+    }
+
+    const isAdmin = user.customRole === 'admin';
+
+    /**
+     * ADMIN → see all tickets
+     */
+    if (isAdmin) {
+      return await super.find(ctx);
+    }
+
+    /**
+     * NON-ADMIN → see only:
+     *  - tickets created by user
+     *  - tickets assigned to user
+     */
+    ctx.query = {
+      ...ctx.query,
+      filters: {
+        $or: [
+          { ticketCreatedBy: user.email },
+          { assignedSME: user.email },
+        ],
+      }
+    };
+
+    return await super.find(ctx);
+  },
+
+  // async findOne(ctx) {
+  //   const user = ctx.state.user;
+  //   const { id } = ctx.params;
+
+  //   if (!user) return ctx.unauthorized();
+
+  //   const ticket = await strapi.entityService.findOne(
+  //     'api::ticket.ticket',
+  //     id,
+  //     {
+  //       populate: ['createdBy', 'assignedSME'],
+  //     }
+  //   );
+
+  //   if (!ticket) return ctx.notFound();
+
+  //   const isAdmin = user.customRole === 'admin';
+
+  //   if (
+  //     !isAdmin &&
+  //     ticket.createdBy?.id !== user.id &&
+  //     ticket.assignedSME?.id !== user.id
+  //   ) {
+  //     return ctx.forbidden();
+  //   }
+
+  //   return ticket;
+  // }
+
 }));
